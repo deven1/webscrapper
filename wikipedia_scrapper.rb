a9ab2455eb44d22f73ff15c6294a62c524e2dbe4
deven1 <deven@sloan.mit.edu>
2015-03-13 13:27:04 -0400

    updated with multiple comments

--- wikipedia_scrapper.rb
+++ wikipedia_scrapper.rb
@@ -1,15 +1,16 @@
 require 'open-uri'
 require 'nokogiri'
 
+# Store URL to be scrapped
 url="http://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
+
+# Parse the page with Nokogiri
 page = Nokogiri::HTML(open(url))
 
+# Display output onto the screen
 puts page.css('a')
-
 puts page.css('li.toclevel-3')
-
 puts page.css('td[style="text-align:left;"]')
-
 puts page.css('td[style="text-align:left;"]').text
 
 page.css('td[style="text-align:left;"]').each do |player|
