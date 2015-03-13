39f7cb8d41f03e124823c023fd95d6e0c8312fb0
deven1 <deven@sloan.mit.edu>
2015-03-13 13:10:51 -0400

    now including several Nokogiri methods

--- wikipedia_scrapper.rb
+++ wikipedia_scrapper.rb
@@ -4,4 +4,14 @@ require 'nokogiri'
 url="http://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
 page = Nokogiri::HTML(open(url))
 
-puts page
+puts page.css('a')
+
+puts page.css('li.toclevel-3')
+
+puts page.css('td[style="text-align:left;"]')
+
+puts page.css('td[style="text-align:left;"]').text
+
+page.css('td[style="text-align:left;"]').each do |player|
+	puts player.text
+end
