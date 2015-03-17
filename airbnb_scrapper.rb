ffcedf9733629c1b84e214ac99791e20ed3504d3
deven1 <deven@sloan.mit.edu>
2015-03-17 11:41:18 -0400

    Added .strip and .split methods to parse data

--- airbnb_scrapper.rb
+++ airbnb_scrapper.rb
@@ -17,18 +17,20 @@ end
 
 price = []
 page.css('span.h3.price-amount').each do |rent|
-	price << rent.text.strip
+	price << rent.text
 end
+# puts price
 
 info = []
 page.css('div.text-muted.listing-location.text-truncate').each do |details|
-	info << details.text.strip
+	info << details.text.strip.split(/ · /)
 end
+# puts info
 
 # Write data to CSV file
 CSV.open("airbnb_listings.csv","w") do |file|
-	file << ["Listing Name", "Rent", "Details"]
+	file << ["Listing Name", "Rent", "Room Type", "Number of Reviews", "Location"]
 	name.length.times do |i|
-		file << [name[i], price[i], info[i]]
+		file << [name[i], price[i], info[i][0], info[i][1], info[i][2]]
 	end
 end		
